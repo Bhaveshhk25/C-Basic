c9b33ebbbb70fd5fe4d5ef7dc9ccc8a8a6fcff81
Bhaveshhk25 <bhaveshhk25@gmail.com>
2018-06-08 20:16:38 +0530

    Using of \t and \n escape sequence in printf

---
+++ l2 printf function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+
+    printf("NAME\t:");
+    printf("BHAVESH\n");
+    printf("FATHER\tNAME:");
+    printf("SAKTHIVEL\n");
+    printf("MOTHER\tNAME:");
+    printf("SASIKALA\n");
+    printf("-------------------------------------------------------------------------\n");
+    printf("\t\t\t\tTHANK YOU\n");
+    printf("PHONE\tNO\t:9443001179");
+}
