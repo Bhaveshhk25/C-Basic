1e022a21881e9f772ba5984085f24ec64cc93cdc
Bhaveshhk25 <bhaveshhk25@gmail.com>
2018-06-08 20:17:40 +0530

    basic of scanf

---
+++ l2 scanf basic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("ENTER A\t:");
+    scanf("%d",&a);
+    printf("ENTER B\t:");
+    scanf("%d",&b);
+    c=(a*a)+(b*b)+(2*a*b);
+    printf("%d",c);
+} 
