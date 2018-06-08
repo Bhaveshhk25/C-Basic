557182b59da92eb6ef1a81658ee5adc1d66f91dc
Bhaveshhk25 <bhaveshhk25@gmail.com>
2018-06-08 20:13:30 +0530

    adding two numbers in printf

---
+++ l2 printf add.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    
+    printf("ENTER YOUR VALUE A\t:\n");
+    scanf("%d",&a);
+    printf("ENTER YOUR VALUE B\t:\n");
+    scanf("%d",&b);
+    printf("%d",a+b);
+   
