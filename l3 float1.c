f953daf164ce40106d1d13467305209ea4089a66
Bhaveshhk25 <bhaveshhk25@gmail.com>
2018-06-14 22:34:53 +0530

    new about float

---
+++ l3 float1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+float akash,aravinth,bhavesh,answer,k;
+printf("ENTER AKASH SALARY\t:");
+scanf("%f",&akash);
+printf("ENTER ARAVINTH SALARY\t:");
+scanf("%f",&aravinth);
+printf("ENTER BHAVESH SALARY\t:");
+scanf("%f",&bhavesh);
+answer=(akash+aravinth+bhavesh)/3;
+printf("%f\n",answer);
+}
+
