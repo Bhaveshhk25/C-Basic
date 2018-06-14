f63341701b4e73aba432ae66a10d285655e03a49
Bhaveshhk25 <bhaveshhk25@gmail.com>
2018-06-14 22:36:19 +0530

    new about if

---
+++ l8 if1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main()
+{
+int x,y;
+
+
+printf("ENTER YOUR VALUE X\t:");
+scanf("%d",&x);
+printf("ENTER YOUR VALUE Y\t:");
+scanf("%d",&y);
+
+if(x==y)
+{
+printf("EQUAL\n");
+}
+
+}
