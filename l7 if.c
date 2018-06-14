2ae78abf307f29b52e336c8c16261f20daa5e523
Bhaveshhk25 <bhaveshhk25@gmail.com>
2018-06-14 22:35:51 +0530

    new about char

---
+++ l7 if.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+int m,n;
+
+printf("ENTER THE VALUE M\t:");
+scanf("%d",&m);
+printf("ENTER THE VALUE N\t:");
+scanf("%d",&n);
+if(m>n);
+{
+printf("GREATER\n");
+}
+}
