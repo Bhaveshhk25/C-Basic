8576de923a8f6615216430e15977b4044fe62849
Bhaveshhk25 <bhaveshhk25@gmail.com>
2018-06-14 22:36:46 +0530

    new about if

---
+++ l9 if2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void main()
+{
+int t,u;
+
+
+printf("ENTER THE VALUE T\t:");
+scanf("%d",&t);
+printf("ENTER THE VALUE U\t:");
+scanf("%d",&u);
+if(t!=u)
+{
+printf("NOT EQUAL\n");
+}
+
+}
